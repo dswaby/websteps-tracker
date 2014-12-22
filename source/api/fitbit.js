7fc4bf995484f0165a3bcbd249dcc68d7eda47a5
Danny Swaby <dswaby@spredfast.com>
2014-12-22 09:16:39 -0600

    updating fitbit endpoint

--- source/api/fitbit.js
+++ source/api/fitbit.js
@@ -4,28 +4,29 @@ var fs = require("fs"),
 
 var pictures = function (app) {
 
-  var p = "./public/img/";
-  var images = [];
-  fs.readdir(p, function (err, files) {
-    if (err) {
-      throw err;
-    }
-    files.map(function (file) {
-      return path.join(p, file);
-    }).filter(function (file) {
-      return fs.statSync(file).isFile();
-    }).forEach(function (file) {
-      console.log(file)
-        if (path.extname(file).toString().toUpperCase() === '.JPG'|| path.extname(file).toString().toUpperCase() === '.PNG' || path.extname(file).toString().toUpperCase() === '.jpeg') {
-          images.push({
-            "path": "./img/"+ path.basename(file),
-            "created": (fs.statSync(file).ctime).getTime()
-          });
-        }
-    });
-  });
+
 
   app.get('/api/fitbit', function (req, res) {
+    var p = "./public/img/";
+    var images = [];
+    fs.readdir(p, function (err, files) {
+      if (err) {
+        throw err;
+      }
+      files.map(function (file) {
+        return path.join(p, file);
+      }).filter(function (file) {
+        return fs.statSync(file).isFile();
+      }).forEach(function (file) {
+        console.log(file)
+          if (path.extname(file).toString().toUpperCase() === '.JPG'|| path.extname(file).toString().toUpperCase() === '.PNG' || path.extname(file).toString().toUpperCase() === '.jpeg') {
+            images.push({
+              "path": "./img/"+ path.basename(file),
+              "created": (fs.statSync(file).ctime).getTime()
+            });
+          }
+      });
+    });
       res.json(images);
   });
 };

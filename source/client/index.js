ea36b0a1d66382151481e3ab76a0db2a26e4bda7
Danny Swaby <dswaby@spredfast.com>
2014-12-21 22:37:46 -0600

    new build

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main.js',
-	css: '/build/main.css'
+	js: '/build/main-196784c1f3ec7dfc0a7009bc9a5ee5fc.js',
+	css: '/build/main-c4ef938aa16fa3db86a4de156f13d9b8.css'
 };

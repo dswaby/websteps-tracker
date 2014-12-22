0e2606d9610ef5721c2c7c8875e07a11cec01a08
Danny Swaby <dswaby@spredfast.com>
2014-12-22 09:11:23 -0600

    whitespace for deploy

--- Gruntfile.js
+++ Gruntfile.js
@@ -150,9 +150,9 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-hashres2');
   grunt.loadNpmTasks('grunt-express-server');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  // grunt.loadNpmTasks('grunt-contrib-coffee');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  // grunt.loadNpmTasks('grunt-contrib-coffee');
 
 
 

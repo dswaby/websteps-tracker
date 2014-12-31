89083eced974bac6f8cde889e52b1fec083891db
Danny Swaby <dswaby@spredfast.com>
2014-12-31 03:10:00 -0600

    deploying

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main.js',
-	css: '/build/main.css'
+	js: '/build/main-61b12a5513ca848ddece5294e5f6df07.js',
+	css: '/build/main-a8bbd725d597545319625c850b1dee8c.css'
 };

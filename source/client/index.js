868928821c980f6d43992632c5fa45196aa830b7
Danny Swaby <dswaby@spredfast.com>
2014-12-31 17:02:59 -0600

    header styles

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main-a81f4fdb43e13ef355dc5ad5415d1e8a.js',
-	css: '/build/main-a515d5b5bb4845372305d7a3576a92fa.css'
+	js: '/build/main.js',
+	css: '/build/main.css'
 };

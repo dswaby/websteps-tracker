343e646970e834c5051d404a1c8bab65ce35ddd5
Danny Swaby <dswaby@spredfast.com>
2014-12-31 21:12:17 -0600

    trying to fix file limit

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(middleware.cors());
 // app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(methodOverride());
 
 if (process.env.NODE_ENV === 'development') {

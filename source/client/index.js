f31d47df226bd4e2aee3f351cc2ae0233dcaac31
Danny Swaby <dswaby@spredfast.com>
2015-01-01 03:02:50 -0600

    updating ignore to ignore generated client index

--- source/client/index.js
+++ source/client/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-	js: '/build/main.js',
-	css: '/build/main.css'
+	js: '/build/main-aa2dde33d286ce8d21b70e955e0edbe4.js',
+	css: '/build/main-d922fca7c4df0ac50dcb3db96202e68b.css'
 };

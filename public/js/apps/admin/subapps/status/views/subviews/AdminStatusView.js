be0340fd5e427e3acdcb5d0c22618f2fd2b69b6f
Danny Swaby <dswaby@spredfast.com>
2015-01-16 12:44:54 -0600

    adding toggle switch

--- public/js/apps/admin/subapps/status/views/subviews/AdminStatusView.js
+++ public/js/apps/admin/subapps/status/views/subviews/AdminStatusView.js
@@ -8,6 +8,7 @@ define(function (require) {
     wnt.steps = localStorage.steps;
   var StatusView = Backbone.View.extend({
     delay: 10,
+    mobile:false,
     steps: 0,
     state: {},
     // falseStepLimit: 25,
@@ -50,13 +51,45 @@ define(function (require) {
     toggleTreadMill: function(){
       var that = this;
       if (that.state.treadmillOn) {
-        that.$el.find("#treadmillToggle").addClass("toggle-on");
+        that.$el.find("#treadmill").addClass("toggle-on");
         that.state.treadmillOn = false;
       }
       else {
-        that.$el.find("#pedometer").html("toggle-off");
+        that.$el.find("#treadmill").html("toggle-off");
         that.state.treadmillOn = true;
         that.socket.emit('on treadmill');
+      }
+    },
+    checkMobile: function() {
+      if( navigator.userAgent.match(/Android/i)
+       || navigator.userAgent.match(/webOS/i)
+       || navigator.userAgent.match(/iPhone/i)
+       || navigator.userAgent.match(/iPad/i)
+       || navigator.userAgent.match(/iPod/i)
+       || navigator.userAgent.match(/BlackBerry/i)
+       ){
+        that.mobile = true;
+      }
+    },
+    trackLocation: function() {
+      if (that.mobile) {
+        var watchid = navigator.geolocation.watchPosition(gotPosition, errorGettingPosition, {'enableHighAccuracy':true,'timeout':10000,'maximumAge':20000});
+      }
+      function gotPosition(pos) {
+        
+      }
+    },
+    toggleLocation: function(){
+      var that = this;
+      if (that.state.locationOn) {
+        that.$el.find("#location").addClass("toggle-off");
+        that.state.locationOn = false;
+      }
+      else {
+        that.$el.find("#location").html("toggle-on");
+        that.checkMobile();
+        that.trackLocation();
+        that.state.treadmillOn = true;
       }
     },
     togglePedometer: function() {
@@ -67,20 +100,21 @@ define(function (require) {
         that.state.pedometerOn = false;
       }
       else {
-        that.startCounting();
+        that.startPedometer();
         that.$el.find("#pedometer").html("pause pedometer");
         that.state.pedometerOn = true;
       }
     },
-    startCounting: function() {
+    startPedometer: function() {
       var that = this;
       var interval = 0;
+      var locationIntervalPasses = 0;
       var accelerationX = 0;  
       var accelerationY = 0;  
       var accelerationZ = 0;  
       var config = {
-        high: 220,
-        low: 70
+        high: 275,
+        low: 0
       };
       var runnningPeak = 700;
       // var falseStepCount = 0;
@@ -102,6 +136,16 @@ define(function (require) {
 
       intervalId = setInterval(function() {
         var plotPoint = (accelerationX * accelerationX) + (accelerationY * accelerationY) + (accelerationZ * accelerationZ);
+        if (that.state.locationOn) {
+          if (locationIntervalPasses === 20) {
+            // send coordinates here
+            that.socket.emit("location updated");
+            locationIntervalPasses = 0;
+          }
+          else {
+            locationIntervalPasses++;
+          }
+        }
         if (state === "low" ) {
           if (plotPoint >= config.high) {
             that.steps++;

993ef4aacdb9b7c291872a435220ade26df728b2
Danny Swaby <dswaby@spredfast.com>
2015-01-16 22:33:33 -0600

    whoops

--- public/js/apps/status/views/subviews/StatusView.js
+++ public/js/apps/status/views/subviews/StatusView.js
@@ -1,6 +1,7 @@
-define(['async!http://maps.google.com/maps/api/js?sensor=false'], function (require) {
+define(function (require) {
   var Backbone = require('Backbone');
   var firstPass = true;
+  require('async!http://maps.google.com/maps/api/js?sensor=false');
   var StatusView = Backbone.View.extend({
     template: require('hbs!./../../templates/StatusView'),
     className: 'location-wrapper',
@@ -47,10 +48,6 @@ define(['async!http://maps.google.com/maps/api/js?sensor=false'], function (requ
         else {
           that.panMap(data.latitude, data.longitude);
         }
-        // var embedString = '<iframe width="300" height="170" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q='+data.latitude+','+data.longitude+'&hl=es;z=14&amp;output=embed"></iframe>';
-        
-        // that.$el.find("#map-view").html(embedString);
-        // console.log('latitude: %s, longitude: %s', data.latitude, data.longitude);
       });
     },
     initializeMap: function(latitude, longitude) {

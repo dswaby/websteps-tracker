c40a8bb3f2b5cfba90db7fcff71bf6a1d1acbd13
Danny Swaby <dswaby@spredfast.com>
2015-01-27 14:10:04 -0600

    derp

--- public/js/apps/data/views/subviews/TrackedRunView.js
+++ public/js/apps/data/views/subviews/TrackedRunView.js
@@ -22,7 +22,7 @@ define(function (require) {
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
-      var map = new google.maps.Map(that.$el.find('#map-canvas'), mapOptions);
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
       var travelPath = new google.maps.Polyline({
         path: that.pathCoordinates,

f5ed80d37523d6062904033e36fb557dc016ab23
Danny Swaby <dswaby@spredfast.com>
2015-01-27 12:46:09 -0600

    should be able to do this shit locally

--- public/js/apps/data/views/subviews/TrackedRunView.js
+++ public/js/apps/data/views/subviews/TrackedRunView.js
@@ -1,16 +1,55 @@
 define(function (require) {
   var Backbone = require('Backbone');
+  require('async!http://maps.google.com/maps/api/js?sensor=false');
 
   var TrackedRunView = Backbone.View.extend({
-    tagName: 'tr',
+
 
     template: require('hbs!./../../templates/TrackedRunView'),
 
     render: function () {
       this.$el.html(this.template(this.model.toJSON()));
+      console.log("model to json",this.model.toJSON());
+      this.createTravelPath();
       return this;
+    },
+    initializeMap: function() {
+
+    },
+    createTravelPath: function() {
+      var that = this;
+      this.pathCoordinates = [];
+      for (var x = 0; x < this.model.get("coordinates"); x++) {
+        console.log("inner for", this);
+        // var latLong = new google.maps.LatLng(latitude, longitude);
+      }
+      // var travelPath = new google.maps.Polyline({
+      //   path: that.locationObj.coordinates,
+      //   strokeColor: '#FF0000',
+      //   strokeOpacity: 1.0,
+      //   strokeWeight: 2
+      // });
+
+      // travelPath.setMap(that.map);
     }
+
   });
 
   return TrackedRunView;
 });
+
+// var myLatLong = new google.maps.LatLng(latitude, longitude);
+//       var mapOptions = {
+//         zoom: 17,
+//         center: myLatLong,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//       };
+//       that.locationObj.coordinates.push(myLatLong);
+
+//       that.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+//       that.marker = new google.maps.Marker({
+//         position: myLatLong,
+//         map: that.map,
+//         title: 'Starting Point!'
+//       });

75373051fb42bcbf4c074c12fcf6079ae4c816bb
Danny Swaby <dswaby@spredfast.com>
2015-02-05 10:52:36 -0600

    styling for data view

--- public/js/apps/data/views/MainView.js
+++ public/js/apps/data/views/MainView.js
@@ -2,6 +2,7 @@ define(function(require) {
 	var Backbone = require('Backbone');
 
 	var TrackedRunsView = require('./subviews/TrackedRunsView');
+  var PicsCollectionView = require('./subviews/PicturesView');
 
 	var MainView = Backbone.View.extend({
     className: 'main-data-view',
@@ -14,6 +15,9 @@ define(function(require) {
 			this.$el.append(trackedRunsView.render().el);
 			this.subviews.push(trackedRunsView);
 
+      var picsCollectionView = new PicsCollectionView({collection: this.model.get("pics")});
+      this.$el.append(picsCollectionView.render().el);
+      this.subviews.push(picsCollectionView);
 			return this;
 		}
 	});
--- public/js/apps/data/views/subviews/PicturesView.js
+++ public/js/apps/data/views/subviews/PicturesView.js
@@ -4,7 +4,7 @@ define(function(require) {
 
   var PicturesView = Backbone.View.extend({
     template: require('hbs!./../../templates/PicturesView'),
-    className: "pictures-view picture-grid",
+    className: "pics-collection-view",
     initialize: function () {
       this.subviews = [];
     },

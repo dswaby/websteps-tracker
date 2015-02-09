0ca0689256d7a5ecc06140b529a06303ec2344c3
Danny Swaby <dswaby@spredfast.com>
2015-02-08 18:47:49 -0600

    added bootstrap

--- public/js/apps/home/views/HeaderView.js
+++ public/js/apps/home/views/HeaderView.js
@@ -5,7 +5,7 @@ define(function (require) {
 		template: require('hbs!./../templates/HeaderView'),
     tagName: 'header',
 		render: function () {
-			this.$el.html(this.template({title: 'Welcome', subtitle: 'This is a proof of concept pedometer and fitness tracking/real-time communications app built using web technologies'}));
+			this.$el.html(this.template({title: 'Step/Geo-location tracking for mobile web browsers', subtitle: 'This is a proof-of-concept pedometer and fitness tracking/real-time communications webapp built using mobile browser technologies'}));
 			return this;
 		}
 	});

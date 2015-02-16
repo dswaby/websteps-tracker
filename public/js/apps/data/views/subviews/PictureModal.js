ae89f906b4fd70d5bc05d6ace2d4220bcbaf7e52
Danny Swaby <dswaby@spredfast.com>
2015-02-15 20:11:38 -0600

    image modal for data view

--- public/js/apps/data/views/subviews/PictureModal.js
+++ public/js/apps/data/views/subviews/PictureModal.js
@@ -2,7 +2,8 @@ define(function (require) {
   var Backbone = require('Backbone');
   require('backboneModal');
   var PictureModal = Backbone.Modal.extend({
-    template: require('hbs!./../../templates/PictureModal')
+    template: require('hbs!./../../templates/PictureModal'),
+    cancelEl: '.dismiss-icon'
   });
 
   return PictureModal;

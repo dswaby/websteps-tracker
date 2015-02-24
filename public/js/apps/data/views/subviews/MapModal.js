079e5f19277bd6aeeb485eca315753284de94f40
Danny Swaby <dswaby@spredfast.com>
2015-02-24 09:25:12 -0600

    changing zoom

--- public/js/apps/data/views/subviews/MapModal.js
+++ public/js/apps/data/views/subviews/MapModal.js
@@ -12,7 +12,7 @@ define(function (require) {
       var that = this;
       var myLatLong = new google.maps.LatLng(latitude, longitude);
       var mapOptions = {
-        zoom: 17,
+        zoom: 10,
         center: myLatLong,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };

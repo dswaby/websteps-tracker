0952f189e4bae2e6b297d7d490295ebeb586d7f1
Danny Swaby <dswaby@spredfast.com>
2015-03-02 05:22:59 -0600

    Deploy Commit

--- public/js/apps/home/app.js
+++ public/js/apps/home/app.js
@@ -4,7 +4,7 @@ define(function(require) {
 	return {
 		run: function (viewManager) {
       var model = new Backbone.Model({
-        title: 'Its like a fitbit in your mobile browser', 
+        title: 'It\'s like a fitbit in your mobile browser', 
         subtitle: 'This is a proof-of-concept pedometer and geo-location app for tracking/real-time communications webapp built using mobile browser technologies'
       });
       var view = new MainView({model: model});
